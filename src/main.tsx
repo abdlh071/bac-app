5c3aec4e736cc1d8c8c84903969994349adc61db
abdlh071 <x79241443@gmail.com>
2025-08-27 17:46:26 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Toaster } from '@/components/ui/toaster'
+import { AuthProvider } from './context/AuthContext.tsx' // <--- إضافة هذا السطر
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <App />
-    <Toaster />
+    {/* <--- إضافة هذا السطر */}
+    <AuthProvider>
+      <App />
+      <Toaster />
+    </AuthProvider>
+    {/* <--- إضافة هذا السطر */}
   </>
 );
