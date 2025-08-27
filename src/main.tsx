67a19e8ba11c75eb9bace89a33ee0e43d62edead
abdlh071 <x79241443@gmail.com>
2025-08-27 18:04:01 +0100

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Toaster } from '@/components/ui/toaster'
-import { AuthProvider } from './context/AuthContext.tsx'
 
-// The root element of the application
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Failed to find the root element");
-}
-
-const root = createRoot(rootElement);
-
-// Render the application, wrapped with the AuthProvider
-root.render(
+createRoot(document.getElementById("root")!).render(
   <>
-    <AuthProvider>
-      <App />
-      <Toaster />
-    </AuthProvider>
+    <App />
+    <Toaster />
   </>
 );
